bff5bf473355a6248f0d33f81eb65db9acb2534e
palexdev <alessandro.parisi406@gmail.com>
2024-05-09 13:28:27 +0200

    Improve WIP Commit detection

--- scripts/gwip.ps1
+++ scripts/gwip.ps1
@@ -3,11 +3,17 @@ function IsGitRepository {
     return (Test-Path -Path ".git" -PathType Container)
 }
 
+# Function to check if the last commit message contains "--wip--"
+function IsLastCommitWIP {
+    $lastCommitMessage = & git log -1 --pretty=%B
+    return $lastCommitMessage -like "*--wip--*"
+}
+
 # Loop to navigate up the directory tree until a Git repository is found
 while (-not (IsGitRepository)) {
     # Change directory up one level
     Set-Location ..
-    
+
     # Check if at root directory
     if ((Get-Location).Path -eq (Get-Location -PSProvider FileSystem).Root) {
         Write-Host "Not in a Git repository" -ForegroundColor red
@@ -16,7 +22,7 @@ while (-not (IsGitRepository)) {
 }
 
 if (IsGitRepository) {
-    Write-Host "Git repository found at: $(Get-Location)" -ForegroundColor green
+    Write-Host "Git repository found at: $( Get-Location )" -ForegroundColor green
 }
 
 Write-Host "Deleting files..." -ForegroundColor green
@@ -26,13 +32,14 @@ Write-Host "Adding files..." -ForegroundColor green
 git add -A
 
 Write-Host "Preparing commit..." -ForegroundColor green
-if ($(git log -1 --pretty=%B) -contains "--wip--") {
-Write-Host "Amending..." -ForegroundColor yellow
-git commit --no-verify --no-gpg-sign --amend -m "--wip-- [skip ci]"
-Write-Host "Force pushing" -ForegroundColor red
-git push -f
-} else {
-git commit --no-verify --no-gpg-sign -m "--wip-- [skip ci]"
-Write-Host "Pushing" -ForegroundColor green
-git push
+if (IsLastCommitWIP) {
+    Write-Host "Amending..." -ForegroundColor yellow
+    git commit --no-verify --no-gpg-sign --amend -m "--wip-- [skip ci]"
+    Write-Host "Force pushing" -ForegroundColor red
+    git push -f
+}
+else {
+    git commit --no-verify --no-gpg-sign -m "--wip-- [skip ci]"
+    Write-Host "Pushing" -ForegroundColor green
+    git push
 }
--- scripts/unwip.ps1
+++ scripts/unwip.ps1
@@ -3,11 +3,17 @@ function IsGitRepository {
     return (Test-Path -Path ".git" -PathType Container)
 }
 
+# Function to check if the last commit message contains "--wip--"
+function IsLastCommitWIP {
+    $lastCommitMessage = & git log -1 --pretty=%B
+    return $lastCommitMessage -like "*--wip--*"
+}
+
 # Loop to navigate up the directory tree until a Git repository is found
 while (-not (IsGitRepository)) {
     # Change directory up one level
     Set-Location ..
-    
+
     # Check if at root directory
     if ((Get-Location).Path -eq (Get-Location -PSProvider FileSystem).Root) {
         Write-Host "Not in a Git repository" -ForegroundColor red
@@ -16,13 +22,14 @@ while (-not (IsGitRepository)) {
 }
 
 if (IsGitRepository) {
-    Write-Host "Git repository found at: $(Get-Location)" -ForegroundColor green
+    Write-Host "Git repository found at: $( Get-Location )" -ForegroundColor green
 }
 
 Write-Host "Searching for wip commit..." -ForegroundColor green
-if ($(git log -1 --pretty=%B) -contains "--wip--") {
-Write-Host "Wip commit found. Resetting..." -ForegroundColor green
-git reset HEAD~1
-} else {
-Write-Host "Wip commit not found" -ForegroundColor red
+if (IsLastCommitWIP) {
+    Write-Host "Wip commit found. Resetting..." -ForegroundColor green
+    git reset HEAD~1
+}
+else {
+    Write-Host "Wip commit not found" -ForegroundColor red
 }
